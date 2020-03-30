089c9efa761d26e86d5f9ff49be53ea625458b06
Emanuel Limeira <emanuellimeira@MacBook-de-Emanuel.local>
2020-03-29 20:51:04 -0400

    licao 4 - object destructuring

---
+++ 04_object_destructuring.js
@@ -0,0 +1,8 @@
+// importar a função, simplificando e retirando apenas a funcao de soma
+const { sum } = require('./02_helpers');
+
+// var let const
+
+const total = sum(10, 200);
+
+console.log("TOTAL: ", total);

7aea0cc36518f9f672a8de0db28ca1996eceb3e5
Emanuel Limeira <emanuellimeira@MacBook-de-Emanuel.local>
2020-03-29 20:45:01 -0400

    licao 3 - arrow functions

---
+++ 03_arrow_function.js
@@ -0,0 +1,14 @@
+// função para somar
+// function sum(a, b) {
+// 	return a + b;
+// }
+
+// uma linha função em uma linha e já exporta
+exports.sum = (a,b) => a + b;
+
+// exporta a função
+// module.exports = {
+
+// 	sum
+
+// }
